645d6160f75653802442dc5e32265715be29dc71
luke lombardi <luke@slai.io>
2022-10-21 14:10:10 -0400

    handle multiple architectures

--- get-beam.sh
+++ get-beam.sh
@@ -1,11 +1,59 @@
 #!/bin/sh
 
-{
+{ # Prevent execution if this script was only partially downloaded
+
         set -e
 
-        install_dir="/usr/local/bin"
-        install_path="$install_dir/beam"
-        latest_release_uri="https://github.com/slai-labs/get-beam/releases"
+        install_dir='/usr/local/bin'
+        install_path='/usr/local/bin/beam'
+        OS=$(uname | tr '[:upper:]' '[:lower:]')
+        ARCH=$(uname -m | tr '[:upper:]' '[:lower:]')
+        cmd_exists() {
+                command -v "$@" >/dev/null 2>&1
+        }
+
+        bin_file=
+
+        case "$OS" in
+        darwin)
+                case "$ARCH" in
+                x86_64)
+                        bin_file=beam-Darwin-x86_64
+                        ;;
+                arm64)
+                        bin_file=beam-Darwin-arm64
+                        ;;
+                *)
+                        printf '\033[31m> The architecture (%s) is not supported by this installation script.\n\033[0m' "$ARCH"
+                        exit 1
+                        ;;
+                esac
+                ;;
+        linux)
+                case "$ARCH" in
+                x86_64)
+                        bin_file=beam-Linux-x86_64
+                        ;;
+                amd64)
+                        bin_file=beam-Linux-x86_64
+                        ;;
+                armv8*)
+                        bin_file=beam-Linux-arm64
+                        ;;
+                aarch64)
+                        bin_file=beam-Linux-arm64
+                        ;;
+                *)
+                        printf '\033[31m> The architecture (%s) is not supported by this installation script.\n\033[0m' "$ARCH"
+                        exit 1
+                        ;;
+                esac
+                ;;
+        *)
+                printf '\033[31m> The OS (%s) is not supported by this installation script.\n\033[0m' "$OS"
+                exit 1
+                ;;
+        esac
 
         sh_c='sh -c'
         if [ ! -w "$install_dir" ]; then
@@ -23,19 +71,20 @@
         fi
 
         echo "Getting latest release"
-        # 
+        latest_release_uri="https://github.com/slai-labs/get-beam/releases"
         latest_release_tag=$(curl -sL -o /dev/null -w %{url_effective} $latest_release_uri/latest | rev | cut -d/ -f1 | rev)
 
         echo "Latest release: $latest_release_tag"
-        latest_beam_binary_uri="$latest_release_uri/download/$latest_release_tag/beam"
+        latest_beam_binary_uri="$latest_release_uri/download/$latest_release_tag/$bin_file"
 
         download_path=$(mktemp)
         echo "Downloading latest beam binary $latest_beam_binary_uri\n"
-        curl -L $latest_beam_binary_uri -o $download_path
-
+        curl -fSL $latest_beam_binary_uri -o $download_path
         chmod +x $download_path
 
+        printf '> Installing %s\n' "$install_path"
         $sh_c "mv -f $download_path $install_path"
 
-        printf "\033[32m\nSuccessfully installed latest version of Beam: $latest_release_tag\n\033[0m"
-}
+        printf '\033[32m> Beam successfully installed!\n\033[0m'
+
+} # End of wrapping
