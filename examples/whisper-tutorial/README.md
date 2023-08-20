353854ce6292bd5c995e4ced2dac3db89df770f4
beta9 <egottli2@binghamton.edu>
2023-08-20 13:17:59 -0400

    whisper tutorial

---
+++ examples/whisper-tutorial/README.md
@@ -0,0 +1,55 @@
+# Test Locally
+
+### Connect to the runtime
+
+```sh
+beam start app.py
+```
+
+### Run your Python code
+
+```
+(beam) python app.py
+```
+
+# Deploy
+
+```sh
+beam deploy app.py:transcribe_audio
+```
+
+# Run Inference
+
+1. Add your Beam client ID and client secret to `request.py`. You can find your Beam credentials [here](https://www.beam.cloud/dashboard/settings/api-keys).
+2. Add your App ID. You can find the app ID in the dashboard, under your app:
+
+![](/img/call-api.png)
+
+![](/img/app-id.png)
+
+
+### Send a request
+
+```python
+python request.py
+```
+
+| Note: REST APIs have a 90s timeout. If your audio file is large, your request may timeout and continue to run asynchronously. In this case, you can retrieve the output from the API in the dashboard, or through the   [`/task`](https://docs.beam.cloud/data/outputs#retrieving-task-outputs) API. Continue reading to see how. 
+
+After you make a request, you'll see a task appear in the dashboard:
+
+![](/img/tasks.png)
+
+
+### View the output file
+
+To view the transcript, click on your task, and click **Outputs**:
+
+![](/img/outputs.png)
+
+If your task finished running successfully, you'll see a file listed. Click the file, and you'll see the text transcript open in your browser window:
+
+![](/img/transcript.png)
+
+
+
---
+++ examples/whisper-tutorial/app.py
@@ -0,0 +1,61 @@
+from beam import App, Runtime, Image, Volume, Output
+import base64
+import whisper
+from whisper.audio import *
+
+from tempfile import NamedTemporaryFile
+import torch
+
+device = "cuda"
+
+app = App(
+    name="whisper",
+    runtime=Runtime(
+        cpu=4,
+        memory="8Gi",
+        gpu="T4",
+        image=Image(
+            python_packages=["git+https://github.com/openai/whisper.git"],
+            commands=["apt-get update && apt-get install -y ffmpeg"],
+        ),
+    ),
+    volumes=[Volume(path="./cache", name="cache")],
+)
+
+
+@app.rest_api(outputs=[Output(path="output.txt")])
+def transcribe_audio(**inputs):
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    # Create a temporary file.
+    with NamedTemporaryFile() as temp:
+        # Write the user's uploaded file to the temporary file.
+        audio_file = base64.b64decode(inputs["audio_file"].encode("utf-8"))
+        temp.write(audio_file)
+        model = whisper.load_model(
+            inputs.get("model", "small"), device=device, download_root="./cache"
+        )
+        result = model.transcribe(temp.name)
+
+        print(result["text"])
+
+        with open("output.txt", "w") as f:
+            f.write(result["text"])
+
+        return {"transcript": result["text"]}
+
+
+if __name__ == "__main__":
+    """'
+    *** Testing Locally ***
+
+    > beam start app.py
+    > python app.py
+
+    """
+    import os
+
+    mp3_filepath = os.path.abspath("example.wav")
+    transcribe_audio(
+        audio_file=base64.b64encode(open(mp3_filepath, "rb").read()).decode("UTF-8"),
+        model="small",
+    )
