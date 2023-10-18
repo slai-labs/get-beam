6172df7a343ece6176a235884dac708dbf8666e3
beta9 <egottli2@binghamton.edu>
2023-10-18 09:29:40 -0400

    clean up

--- examples/pygmalion/app.py
+++ examples/pygmalion/app.py
@@ -1,13 +1,20 @@
-# Run this by running: beam serve app.py:predict
+"""
+
+*** Serverless Pygmalion API ***
+
+> Run the API: beam serve app.py:predict
+> Deploy it: beam deploy app.py:predict
+
+"""
 
 from beam import App, Runtime, Image, Volume, RequestLatencyAutoscaler
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-# Beam Volume to store cached models
+# Beam volume to store cached models
 CACHE_PATH = "./cached_models"
 
 app = App(
-    name="pygmalian",
+    name="pygmalion",
     runtime=Runtime(
         cpu=2,
         memory="32Gi",
@@ -24,15 +31,12 @@ app = App(
             ],  # You can also add a path to a requirements.txt instead
         ),
     ),
-    # Storage Volume for model weights
+    # Storage volume for cached models
     volumes=[Volume(name="cached_models", path=CACHE_PATH)],
 )
 
-# Autoscale by request latency - will spin up add'l replicas if latency exceeds 30s
-autoscaler = RequestLatencyAutoscaler(desired_latency=30, max_replicas=5)
 
-
-# This function runs once when the container boots
+# Pre-load models: this function runs once when the container boots
 def load_models():
     tokenizer = AutoTokenizer.from_pretrained(
         "PygmalionAI/pygmalion-6b", cache_dir=CACHE_PATH
@@ -46,16 +50,20 @@ def load_models():
     return model, tokenizer
 
 
+# Autoscale by request latency - will spin up add'l replicas if latency exceeds 30s
+autoscaler = RequestLatencyAutoscaler(desired_latency=30, max_replicas=5)
+
+
 # Rest API initialized with loader and autoscaler
 @app.rest_api(loader=load_models, autoscaler=autoscaler)
 def predict(**inputs):
     # Retrieve cached model from loader
     model, tokenizer = inputs["context"]
-
+    # Input from API request
     prompt = inputs["prompt"]
-    inputs = tokenizer(prompt, return_tensors="pt")
 
-    # Generate
+    # Inference
+    inputs = tokenizer(prompt, return_tensors="pt")
     generate_ids = model.generate(inputs.input_ids.to("cuda"), max_length=30)
     result = tokenizer.batch_decode(
         generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False
