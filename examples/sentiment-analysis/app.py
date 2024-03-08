9b67d59fc7beb15d1ad77e66fc2591644e80f803
beta9 <egottli2@binghamton.edu>
2024-03-07 20:43:12 -0500

    add placeholder input

--- examples/sentiment-analysis/app.py
+++ examples/sentiment-analysis/app.py
@@ -4,8 +4,8 @@ from transformers import pipeline
 app = App(
     name="sentiment-analysis",
     runtime=Runtime(
-        cpu=4,
-        memory="32Gi",
+        cpu=2,
+        memory="16Gi",
         image=Image(
             python_version="python3.9",
             python_packages=["transformers", "torch"],
@@ -26,7 +26,13 @@ def predict_sentiment(**inputs):
     # Retrieve model from loader
     model = inputs["context"]
 
-    result = model(inputs["text"], truncation=True, top_k=1)
+    try:
+        text = inputs["text"]
+    # Use a default input if none is provided
+    except KeyError:
+        text = "I love being outside on a nice day"
+
+    result = model(text, truncation=True, top_k=1)
     prediction = {i["label"]: i["score"] for i in result}
 
     print(prediction)
